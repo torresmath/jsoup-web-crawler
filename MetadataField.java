deaf1e914482061bb9fb7ccd2347f37440f12ef3
torresmath <35973434+torresmath@users.noreply.github.com>
2020-04-22 19:28:41 -0300

    Create MetadataField.java

---
+++ MetadataField.java
@@ -0,0 +1,9 @@
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class MetadataField implements Serializable {
+    private String title;
+    private String description;
+    private String image;
+}
